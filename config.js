e7fdff084e5f4fd888735b6c9d5b058ef2e0f39a
MOONKIGHT <102457400+MOONKIGHT@users.noreply.github.com>
2022-05-30 22:59:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'YourApikey',
+  'https://api.xteam.xyz': 'NezukoTachibana281207',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
